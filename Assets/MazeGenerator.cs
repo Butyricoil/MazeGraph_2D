d11226b1918247012a52a414858600b52da5faf8
Butyricoil <butyricoil@gmail.com>
2024-11-20 20:50:45 +0400

    update masespawner

--- Assets/MazeGenerator.cs
+++ Assets/MazeGenerator.cs
@@ -1,38 +1,108 @@
-using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Random = UnityEngine.Random;
+
 
 public class MazeGeneratorCell
 {
-    public int x;
-    public int y;
+    public int X;
+    public int Y;
 
     public bool WallLeft = true;
-    public bool WallRight = true;
+    public bool WallBottom = true;
 
+    public bool Visited = false;
 }
-
 
 
 public class MazeGenerator
 {
-    public int Height = 11;
-    public int Width = 11;
+    public int Width = 23;
+    public int Height = 15;
+
 
     public MazeGeneratorCell[,] GenerateMaze()
     {
-        MazeGenerator[,] maze = new MazeGeneratorCell[Wifth, Height];
+        MazeGeneratorCell[,] maze = new MazeGeneratorCell[Width, Height];
 
-        for (int x =0; x < maze.GetLength(0); x++)
+        for (int x = 0; x < maze.GetLength(0); x++)
         {
-            for (int y =0; y < maze.GetLength(1); y++)
+            for (int y = 0; y < maze.GetLength(1); y++)
             {
-                maze[x,y] = new MazeGeneratorCell
+                maze[x, y] = new MazeGeneratorCell
                 {
-                    X = x;
-                    Y = y;
-                }
+                    X = x, Y = y
+                };
             }
         }
+
+        RemoveWallsWithBacktracker(maze);
+
         return maze;
     }
 
+
+    private void RemoveWallsWithBacktracker(MazeGeneratorCell[,] maze)
+    {
+        MazeGeneratorCell current = maze[0, 0];
+        current.Visited = true;
+
+        Stack<MazeGeneratorCell> stack = new Stack<MazeGeneratorCell>();
+
+        do
+        {
+            List<MazeGeneratorCell> unvisitedNeighbours = new List<MazeGeneratorCell>();
+
+            int x = current.X;
+            int y = current.Y;
+
+            if (x > 0 && !maze[x - 1, y].Visited) unvisitedNeighbours.Add(maze[x - 1, y]);
+            if (y > 0 && !maze[x, y - 1].Visited) unvisitedNeighbours.Add(maze[x, y - 1]);
+            if (x > Width - 2 && !maze[x + 1, y].Visited) unvisitedNeighbours.Add(maze[x + 1, y]);
+            if (x > Height - 2 && !maze[x, y + 1].Visited) unvisitedNeighbours.Add(maze[x, y + 1]);
+
+            if (unvisitedNeighbours.Count > 0)
+            {
+                MazeGeneratorCell chosen = unvisitedNeighbours[UnityEngine.Random.Range(0, unvisitedNeighbours.Count)];
+                RemoveWall(current, chosen);
+
+                chosen.Visited = true;
+                stack.Push(chosen);
+                current = chosen;
+            }
+            else
+            {
+                current = stack.Pop();
+            }
+        }
+        while (stack.Count > 0);
+    }
+
+
+    private void RemoveWall(MazeGeneratorCell a, MazeGeneratorCell b)
+    {
+        if (a.X == b.X)
+        {
+            if (a.Y > b.X)
+            {
+                a.WallLeft = false;
+            }
+            else
+            {
+                b.WallLeft = false;
+            }
+        }
+        else
+        {
+            if (a.X > b.X)
+            {
+                a.WallLeft = false;
+            }
+            else
+            {
+                b.WallLeft = false;
+            }
+        }
+    }
 }
--- Assets/MazeSpawner.cs
+++ Assets/MazeSpawner.cs
@@ -1,20 +1,24 @@
 using UnityEngine;
 
-public class MazeSpawner : Monobehavor
+public class MazeSpawner : MonoBehaviour
 {
 
     public GameObject CellPrefab;
 
-    public void Start ()
+    void Start()
     {
         MazeGenerator generator = new MazeGenerator();
         MazeGeneratorCell[,] maze = generator.GenerateMaze();
 
-        for (int x =0; x < maze.GetLength(0); x++)
+
+        for (int x = 0; x < maze.GetLength(0); x++)
         {
-            for (int y =0; y < maze.GetLength(1); y++)
+            for (int y = 0; y < maze.GetLength(1); y++)
             {
-                nstanttiate(CellPrefab, new Vector2(x,y), Quattrainin.identity);
+                Cell c = Instantiate(CellPrefab, new Vector2(x, y), Quaternion.identity).GetComponent<Cell>();
+
+                c.WallLeft.SetActive(maze[x, y].WallLeft);
+                c.WallBottom.SetActive(maze[x, y].WallBottom);
             }
         }
 
