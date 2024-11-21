6519a255fb1c267b0d25c9d75c7bded814c76720
Butyricoil <butyricoil@gmail.com>
2024-11-21 21:44:56 +0400

    maze generator Update

--- Assets/MazeGenerator.cs
+++ Assets/MazeGenerator.cs
@@ -19,7 +19,7 @@ public class MazeGeneratorCell
 public class MazeGenerator
 {
     public int Width = 23;
-    public int Height = 15;
+    public int Height = 10;
 
 
     public MazeGeneratorCell[,] GenerateMaze()
@@ -35,6 +35,16 @@ public class MazeGenerator
                     X = x, Y = y
                 };
             }
+        }
+
+        for (int x = 0; x < maze.GetLength(0); x++)
+        {
+            maze[x, Height - 1].WallLeft = false;
+        }
+
+        for (int y = 0; y < maze.GetLength(1); y++)
+        {
+            maze[Width - 1, y].WallBottom = false;
         }
 
         RemoveWallsWithBacktracker(maze);
@@ -57,10 +67,25 @@ public class MazeGenerator
             int x = current.X;
             int y = current.Y;
 
-            if (x > 0 && !maze[x - 1, y].Visited) { unvisitedNeighbours.Add(maze[x - 1, y]); }
-            if (y > 0 && !maze[x, y - 1].Visited) { unvisitedNeighbours.Add(maze[x, y - 1]); }
-            if (x < Width - 1 && !maze[x + 1, y].Visited) { unvisitedNeighbours.Add(maze[x + 1, y]); }
-            if (y < Height - 1 && !maze[x, y + 1].Visited) { unvisitedNeighbours.Add(maze[x, y + 1]); }
+            if (x > 0 && !maze[x - 1, y].Visited)
+            {
+                unvisitedNeighbours.Add(maze[x - 1, y]);
+            }
+
+            if (y > 0 && !maze[x, y - 1].Visited)
+            {
+                unvisitedNeighbours.Add(maze[x, y - 1]);
+            }
+
+            if (x < Width - 1 && !maze[x + 1, y].Visited)
+            {
+                unvisitedNeighbours.Add(maze[x + 1, y]);
+            }
+
+            if (y < Height - 1 && !maze[x, y + 1].Visited)
+            {
+                unvisitedNeighbours.Add(maze[x, y + 1]);
+            }
 
             if (unvisitedNeighbours.Count > 0)
             {
@@ -84,13 +109,25 @@ public class MazeGenerator
     {
         if (a.X == b.X)
         {
-            if (a.Y > b.X) { a.WallBottom = false; }
-            else { b.WallBottom = false; }
+            if (a.Y > b.X)
+            {
+                a.WallBottom = false;
+            }
+            else
+            {
+                b.WallBottom = false;
+            }
         }
         else
         {
-            if (a.X > b.X) { a.WallLeft = false; }
-            else { b.WallLeft = false; }
+            if (a.X > b.X)
+            {
+                a.WallLeft = false;
+            }
+            else
+            {
+                b.WallLeft = false;
+            }
         }
     }
 }
